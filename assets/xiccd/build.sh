5318d7513bf0979d62e61fca400d65584ba6821a
Alberto Salvia Novella <es20490446e@gmail.com>
2021-03-26 20:41:46 +0100

    build.sh complies with shellcheck

--- assets/xiccd/build.sh
+++ assets/xiccd/build.sh
@@ -1,39 +1,46 @@
 #! /bin/bash
 
+here="$(realpath "$(dirname "${0}")")"
+
 
 mainFunction () {
-	silently "rm" "rm --recursive --force build"
-	silently "cp" "cp --recursive sources build"
-	cd build
-
-	silently "aclocal" "aclocal"
-	silently "automake" "automake --add-missing --foreign"
-	silently "autoconf" "autoconf"
-	silently "configure" "./configure"
-	silently "make" "make"
-
-	silently "mv" "mv xiccd ../xiccd"
-	cd ..
-	silently "rm" "rm --recursive build"
+	createBlankBuildDir
+	build
+	so "mainFunction: moveBinary" mv "${here}/build/xiccd" "${here}/xiccd"
+	so "mainFunction: removeBuild" rm --recursive "${here}/build"
 }
 
 
-changeToThisProgramDir () {
-	cd "$( dirname "${BASH_SOURCE[0]}" )"
+build () {
+	cd "${here}/build"
+	so "build: macros" aclocal
+	so "build: configuration" autoconf
+	so "build: environment" automake --add-missing --foreign --force --copy
+	so "build: builder" ./configure
+	so "build: program" make
 }
 
 
-silently () {
-	function="${1}"
-	command="${2}"
-	error=$(eval "${command}" 2>&1 >"/dev/null")
+createBlankBuildDir () {
+	so "cleanBuildDir: remove" rm --recursive --force "${here}/build"
+	so "cleanBuildDir: create" cp --recursive sources "${here}/build"
+}
 
-	if [ ${?} -ne 0 ]; then
-		echo "${function}: ${error}" >&2
+
+so () {
+	local tag="${1}"
+	local commands="${*:2}"
+	
+	if ! error="$(eval "${commands}" 2>&1 >"/dev/null")" ; then
+		if [ "${error}" == "" ] ; then
+			error="Command failed: ${commands}"
+		fi
+
+		echo "${tag}: ${error}" >&2
 		exit 1
 	fi
 }
 
 
-changeToThisProgramDir
+set -e
 mainFunction
